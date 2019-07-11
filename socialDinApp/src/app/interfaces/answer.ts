497f15879423b0caeedfc93a7a42d3208596c435
Pochu <pochuelo15@gmail.com>
2019-07-11 11:52:34 +0200

    build array of answers

---
+++ socialDinApp/src/app/interfaces/answer.ts
@@ -0,0 +1,6 @@
+export interface Answer {
+  Email: string;
+  QuestionId: number;
+  Answer: string;
+  AnswerType: string;
+}
--- socialDinApp/src/app/question-list/question-list.component.ts
+++ socialDinApp/src/app/question-list/question-list.component.ts
@@ -1,3 +1,5 @@
+import { Answer } from './../interfaces/answer';
+import { Router } from '@angular/router';
 import { FormService } from './../services/form.service';
 import { Component, OnInit } from '@angular/core';
 import { Questions } from '../interfaces/questions';
@@ -9,10 +11,10 @@ import { Questions } from '../interfaces/questions';
 })
 export class QuestionListComponent implements OnInit {
 
-  constructor(private dinapp: FormService, ) { }
+  constructor(private dinapp: FormService, private router: Router) { }
 
   hasLoaded = false;
-  test = false;
+  def = false;
   questions: [Questions];
 
   ngOnInit() {
@@ -22,21 +24,56 @@ export class QuestionListComponent implements OnInit {
 
   submit(answers) {
     console.log(answers.value);
+    const email = localStorage.getItem('EMAIL');
+    console.log(email);
+    // BUILD JSON ARRAY
+    const array: Answer[] = [];
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.questions.length; i++) {
+      let answer = '';
+      // MIX IN ALL KEYS
+      if (this.questions[i].QustionType === '1' || this.questions[i].QustionType === '0') {
+        for (let j = 0; j < this.questions[i].Options.length; j++) {
+          const test = `${i}-${j}`;
+          if (answers.value[test]) {
+            answer === '' ? answer = '' : answer += ', ';
+            answer += `${this.questions[i].Options[j].OptionDescription}`;
+          }
+        }
+      } else {
+        answer = answers.value[i];
+      }
+      const ans = ({
+        Email: email,
+        QuestionId: this.questions[i].QuestionId,
+        Answer: answer,
+        AnswerType: this.questions[i].QustionType,
+      }) as Answer;
+      array.push(ans);
+    }
+    console.log(array);
   }
 
   loadQuestions() {
     const id = localStorage.getItem('FORM');
-    this.dinapp.getForm(id).subscribe((data: [{
-      FormId: number,
-      ConflictId: number,
-      description: string,
-    }]) => {
-      console.log(data);
-      this.dinapp.getQuestions(data[0].FormId).subscribe((questions: [Questions]) => {
-        this.questions = questions;
-        console.log(questions);
+    if (id === null) {
+      this.router.navigateByUrl('');
+    } else {
+      this.dinapp.getForm(id).subscribe((data: [{
+        FormId: number,
+        ConflictId: number,
+        description: string,
+      }]) => {
+        this.dinapp.getQuestions(data[0].FormId).subscribe((questions: [Questions]) => {
+          this.questions = questions;
+          console.log(questions);
+        }, () => {
+          this.router.navigateByUrl('');
+        });
+      }, () => {
+        this.router.navigateByUrl('');
       });
-    });
+    }
   }
 
 }
